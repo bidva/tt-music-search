931aa505a6f182fa947d26c8f2a3b348b5c2bb95
amir bidva <a.bidva@gmail.com>
2017-11-20 22:36:52 -0500

    static representation of ui

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2 className="App-title">Welcome to React with redux</h2>
+        </div>
+        <div className="Search-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Album-List">
+            <ul>
+              <li>
+                <lable>album 01</lable>
+              </li>
+              <li>
+                <lable>album 02</lable>
+              </li>
+              <li>
+                <lable>album 03</lable>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
